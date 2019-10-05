UPBGE/upbge

UPBGE: Fix image tile animation.
Previously the Image::lastupdate was to nan after the convertion and
caused the test in GPU_update_image_time to be discarded.
To fix this issue at read time in the function blo_end_image_pointer_map
lastupdate is set to 0.0f.

Bug reported by Spoopy and still in BFBGE.

source/blender/blenloader/intern/readfile.c
@@ -1656,6 +1656,7 @@ void blo_end_image_pointer_map(FileData *fd, Main *oldmain)
 	}
 	
 	for (; ima; ima = ima->id.next) {
+		ima->lastupdate = 0.0f;
 		ima->cache = newimaadr(fd, ima->cache);
 		if (ima->cache == NULL) {
 			ima->tpageflag &= ~IMA_GLBIND_IS_DATA;

UnidayStudio/blender

source/blender/blenloader/intern/readfile.c
@@ -1951,6 +1951,7 @@ void blo_end_image_pointer_map(FileData *fd, Main *oldmain)
   }
 
   for (; ima; ima = ima->id.next) {
+    ma->lastupdate = 0.0f;
     ima->cache = newimaadr(fd, ima->cache);
     if (ima->cache == NULL) {
       ima->gpuflag = 0;